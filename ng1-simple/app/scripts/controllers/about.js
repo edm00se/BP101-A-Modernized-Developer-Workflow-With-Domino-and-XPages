6a36cf4fa5e32536f9d958ec6c8fc1c0d33aee11
Eric McCormick <edm00se@gmail.com>
2016-07-25 13:54:07 -0500

    added ng1 simple demo

---
+++ ng1-simple/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ng1SimpleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ng1SimpleApp
+ */
+angular.module('ng1SimpleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ ng1-simple/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ng1SimpleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ng1SimpleApp
+ */
+angular.module('ng1SimpleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
